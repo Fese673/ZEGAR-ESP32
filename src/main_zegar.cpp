5e4a700ac282a8a866b490c78211a130fb24c1c5
Remigiusz <remikremik58@gmail.com>
2026-01-20 17:52:24 +0100

    Remove debounce code from main - now handled by Encoder library with 200ms default

--- src/main_zegar.cpp
+++ src/main_zegar.cpp
@@ -51,9 +51,6 @@ const int melodyLen = 4;
 #define ENC_DT 26
 #define ENC_SW 27
 
-// --- Debounce dla enkodera ---
-#define ENCODER_DEBOUNCE_MS 200
-
 // --- UART / Komunikacja ---
 #define UART_BAUD 115200
 HardwareSerial &uart = Serial2;
@@ -114,9 +111,6 @@ bool stm32Connected = false;
 bool stoperRunning = false;
 unsigned long stoperStart = 0, stoperElapsed = 0;
 unsigned long lastStoperDraw = 0;
-
-// --- Debounce Enkodera ---
-unsigned long lastEncoderEvent = 0;
 
 // --- Czas (HH:MM:SS) ---
 int hours = 12, minutes = 0, seconds = 0;
@@ -422,11 +416,7 @@ void loop() {
   // ===== NOWE: Enkoder + UI (zamiast handleEncoder/Button) =====
   EncoderEvent evt = encoder_update();
   if (evt != ENC_NONE) {
-    // Debounce: ignoruj eventy przychodzące zbyt szybko
-    if (millis() - lastEncoderEvent >= ENCODER_DEBOUNCE_MS) {
-      lastEncoderEvent = millis();
-      ui_handleEvent(evt);
-    }
+    ui_handleEvent(evt);
   }
 
   tickClock();
